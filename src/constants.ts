159e32c3e1abec9da899f8e9666a07e975c78998
koufeng <feng.kou@hfin.io>
2023-08-10 10:29:04 +0800

    beta1

--- src/constants.ts
+++ src/constants.ts
@@ -57,3 +57,4 @@ export const SOLIDITY_TYPE_MAXIMA = {
   [SolidityType.uint8]: JSBI.BigInt('0xff'),
   [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 }
+
